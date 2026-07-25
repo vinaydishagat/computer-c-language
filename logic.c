8e0d79f1c42cf1789601e5cfff5658b78b0fd3d9
vinaydishagat <vinaydishagat9971@gmai.com>
2026-07-25 23:17:08 +0530

    c file is added

---
+++ logic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+    int number = 10;
+
+    do {
+        printf("This prints once even though 10 is not less than 5!\n");
+    } while (number < 5);
+
+
+
+    return 0;
+}
