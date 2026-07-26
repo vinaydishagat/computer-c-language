d1491c6f3e17f226d5d575293ed142b27596662e
vinaydishagat <vinaydishagat9971@gmai.com>
2026-07-26 21:55:46 +0530

    CPP file is added

---
+++ build.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  cout<<"Enter your rank:-";
+  cin>>a;
+
+  if (a<=1 && a>=100)
+  {
+    cout<<"Your rank is in merit list : VJTI/SPIT/SPPU/PCCOE"<<"\n";
+  }
+
+  else if(a<=101 && a>200)
+  {
+    cout<<"Your rank is in good list: MET/VESIT/COE/NMITD"<<"\n";
+  }
+
+    return 0;
+}
