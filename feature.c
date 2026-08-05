44f8749c385f5387a9db72633b0278d05581fd02
vinaydishagat <vinaydishagat9971@gmai.com>
2026-08-05 15:24:16 +0530

    mistake

--- feature.c
+++ feature.c
@@ -1,13 +1,1 @@
-#include<stdio.h>
-int main()
-{
-  
-    int a;
-    printf("------------INR to DOLLAR-------------\n");
-    printf("Enter the amout in INR:-");
-    scanf("%d",&a);
-
-    printf("Amount in usd dollar:-%d",90* a);
-
-    return 0;
-}
+//this is empty
