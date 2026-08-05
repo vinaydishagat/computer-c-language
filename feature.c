338ad8c612bdd455431c729f21657788129be4da
vinaydishagat <vinaydishagat9971@gmai.com>
2026-08-05 15:42:26 +0530

    undo by restore lost

--- feature.c
+++ feature.c
@@ -1,1 +1,13 @@
-//this is empty
+#include<stdio.h>
+int main()
+{
+  
+    int a;
+    printf("------------INR to DOLLAR-------------\n");
+    printf("Enter the amout in INR:-");
+    scanf("%d",&a);
+
+    printf("Amount in usd dollar:-%d",90* a);
+
+    return 0;
+}
